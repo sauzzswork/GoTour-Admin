8aee40e594d27a05dbe7cd9539f353d0c22448e9
Saurav Mishra <sm23052003@gmail.com>
2025-12-17 00:02:34 +0530

    Update api.js

--- apps/frontend/src/services/api.js
+++ apps/frontend/src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const API_URL = process.env.REACT_APP_API_URL;
 
 const api = axios.create({
-  baseURL: API_URL,
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 // Add token to requests
