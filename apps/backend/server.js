e1c3475043bdc4e079a68bf35eb702d5ba0b8726
Saurav Mishra <sm23052003@gmail.com>
2025-12-17 00:21:32 +0530

    Update server.js

--- apps/backend/server.js
+++ apps/backend/server.js
@@ -9,7 +9,8 @@ const app = express();
 app.use(cors({
    origin: [
     'http://localhost:3000',
-    'https://go-tour-admin-frontend.vercel.app'
+    'https://go-tour-admin-frontend-mq0cqgb8-saurav-mishras-projects.vercel.app
+'
   ],
   credentials: true
 }));
