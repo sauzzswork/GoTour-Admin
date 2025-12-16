5fe42a9b75aa4c6f9a4ac2b7668e7f61f031bb98
Saurav Mishra <sm23052003@gmail.com>
2025-12-17 00:10:28 +0530

    Update server.js

--- apps/backend/server.js
+++ apps/backend/server.js
@@ -26,7 +26,7 @@ app.get('/api/health', (req, res) => {
 
 // Connect to MongoDB
 mongoose
-  .connect(process.env.MONGODB_URI)
+  .connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch((err) => console.error('MongoDB connection error:', err));
 
